f89f2621bfc34f080de850e7d5f8bf8eb99ff63f
Ahmed Sheikh <56453304+ThatBeardedGuy@users.noreply.github.com>
2021-06-02 15:45:08 -0700

    added conditional rendering and configured filters

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-const App = () => {
-  const expenses = [
-    { title: "Standup Desk", amount: 494.67, date: new Date(2021, 2, 28) },
-    { title: "Car Insurance", amount: 1294.67, date: new Date(2021, 2, 22) },
-    { title: "Phone", amount: 244.67, date: new Date(2021, 2, 18) },
-    { title: "Food", amount: 300.0, date: new Date(2021, 2, 8) },
-  ];
 
-  const addExpenseHandler = (event) => {
-    const saveNewExpenseData = {};
+const DUMMY_EXPENSE = [
+  { id: 1, title: "Standup Desk", amount: 494.67, date: new Date(2021, 2, 28) },
+  {
+    id: 2,
+    title: "Car Insurance",
+    amount: 1294.67,
+    date: new Date(2021, 2, 22),
+  },
+  { id: 3, title: "Phone", amount: 244.67, date: new Date(2021, 2, 18) },
+  { id: 4, title: "Food", amount: 300.0, date: new Date(2021, 2, 8) },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      console.log(expense, prevExpenses);
+      console.log(expenses);
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 };
